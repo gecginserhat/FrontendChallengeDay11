ce3a67b8a79805dd0c1a4fa6ced97465dc927385
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-06 22:26:24 +0300

    Add files via upload

---
+++ 13.Switch Case/app.js
@@ -0,0 +1,35 @@
+
+// if (process === 1) {
+//     console.log("Process 1");
+// }
+// else if (process === 2) {
+//     console.log("Process 2");
+// }
+// else if (process === 3) {
+//     console.log("Process 3");
+// }
+// else {
+//     console.log("Invalid Process");
+// }
+
+const process = 2;
+
+
+switch (process) {
+    case 1:
+        console.log("Process 1");
+        break;
+    case 2:
+        console.log("Process 2");
+        break;
+    case 3:
+        console.log("Process 3");
+        break;
+    default:
+        console.log("Invalid Process");
+        break;
+}
+
+
+
+
